a0fb018ae0bf1db7a438dbb54620bf83e1455a40
RJ-Taylor1 <rjtaylor@southern.edu>
2026-03-19 13:18:21 -0400

    Add ClassLocation and TransitInfo classes

---
+++ models.py
@@ -0,0 +1,9 @@
+class ClassLocation:
+    def __init__(self, building, room):
+        self.building = building
+        self.room = room
+
+class TransitInfo:
+    def __init__(self, bus_number, arrival_time):
+        self.bus_number = bus_number
+        self.arrival_time = arrival_time
